ca35d317bae21f5860adff1e061632280405492c
Sharmin <sharmin@outlook.com.au>
2021-01-13 04:19:59 +0000

    Fixed function names to reflect function content

--- GoogleMeetup/src/main/webapp/script.js
+++ GoogleMeetup/src/main/webapp/script.js
@@ -13,15 +13,15 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Retrieves list of hotspots from the NSW data website.
  */
-async function addRandomGreeting() {
+async function findHotspots() {
   var jsonURL = "";
+  var hotspots = {};
   await fetch('https://data.nsw.gov.au/data/dataset/nsw-covid-19-case-locations/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/view/c258e433-a795-47ea-9dfb-e129d03119e9')
     .then(async function (response) {
       if (response.status == 200){
         var parser = new DOMParser();
-        // Parse the text
         var doc = parser.parseFromString(await response.text(), "text/html");
         const scripts = doc.querySelectorAll("script");
         for(let i = 0; i < scripts.length; i++) {
@@ -37,10 +37,8 @@ async function addRandomGreeting() {
   await fetch("https://" + jsonURL.toString())
     .then(async function (response) {
       if (response.status == 200){
-        // Parse the text
         const data = await response.json();
-        const hotspots = data.data.monitor;
-        console.log(hotspots);
+        hotspots = data.data.monitor;
       }
   });
 }
--- GoogleMeetup/target/GoogleMeetup-1/script.js
+++ GoogleMeetup/target/GoogleMeetup-1/script.js
@@ -13,20 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Retrieves list of hotspots from the NSW data website.
  */
-async function addRandomGreeting() {
-  //const response = await fetch('https://data.nsw.gov.au/data/dataset/nsw-covid-19-case-locations/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9');
-  const greetingContainer = document.getElementById('greeting-container');
-  //console.log(response.text());
+async function findHotspots() {
   var jsonURL = "";
+  var hotspots = {};
   await fetch('https://data.nsw.gov.au/data/dataset/nsw-covid-19-case-locations/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/view/c258e433-a795-47ea-9dfb-e129d03119e9')
     .then(async function (response) {
       if (response.status == 200){
         var parser = new DOMParser();
-        // Parse the text
         var doc = parser.parseFromString(await response.text(), "text/html");
-        //console.log(await response.text());
         const scripts = doc.querySelectorAll("script");
         for(let i = 0; i < scripts.length; i++) {
             if (scripts[i].innerText.includes("data.nsw")) {
@@ -41,10 +37,8 @@ async function addRandomGreeting() {
   await fetch("https://" + jsonURL.toString())
     .then(async function (response) {
       if (response.status == 200){
-        // Parse the text
         const data = await response.json();
-        const hotspots = data.data.monitor;
-        console.log(hotspots);
+        hotspots = data.data.monitor;
       }
   });
 }
