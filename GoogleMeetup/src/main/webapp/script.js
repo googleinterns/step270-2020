136a74aca549de8aeab50f246219473d4b3250f5
Sharmin <sharmin@outlook.com.au>
2021-01-15 02:20:04 +0000

    Removed duplicate statement

--- GoogleMeetup/src/main/webapp/script.js
+++ GoogleMeetup/src/main/webapp/script.js
@@ -18,7 +18,7 @@
 async function findHotspots() {
   var today = new Date();
   var date = String(today.getFullYear()) + String(today.getMonth() + 1).padStart(2, '0') + String(today.getDate()).padStart(2, '0');
-  var today = new Date();
+
   await fetch("https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/covid-case-locations-" + date + "a.json")
     .then(async function (response) {
       if (response.status == 200){
